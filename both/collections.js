60b6b71725078fb5371c7e6936facd73c8cc133a
Aketos <lagardemathieu@hotmail.fr>
2019-12-01 12:52:11 +0100

    first commit - set config forms

---
+++ both/collections.js
@@ -0,0 +1,4 @@
+BotConfig = new Mongo.Collection('bot_configuration');
+ApiConfig = new Mongo.Collection('private_configuration');
+BuyOrders = new Mongo.Collection('buy_orders');
+SellOrders = new Mongo.Collection('sell_orders');
---
+++ client/main.js
@@ -0,0 +1,51 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.main.onCreated(function helloOnCreated() {
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
+});
+
+
+Template.main.helpers({
+    isApiConfigSet() {
+        return ApiConfig.find().count() != 0;
+    },
+});
+
+
+//Template.main.events({
+//    'click button'(event, instance) {
+//        // increment the counter when button is clicked
+//        instance.counter.set(instance.counter.get() + 1);
+//        Test.insert({
+//            testValue: instance.counter.get()
+//        })
+//    },
+//});
+
+Template.privateConfig.events({
+    'submit .save-api-config'(event, instance) {
+        ApiConfig.insert({
+            privateKey: event.target.privateKey.value,
+            publicKey: event.target.publicKey.value
+        })
+    },
+});
+
+Template.botConfig.events({
+    'submit .save-bot-config'(event, instance) {
+        BotConfig.insert({
+            pairAToken: event.target.pairAToken.value,
+            pairABalance: event.target.pairABalance.value,
+            pairBToken: event.target.pairBToken.value,
+            pairBBalance: event.target.pairBBalance.value,
+            minPriceExpected: event.target.minPriceExpected.value,
+            maxPriceExpected: event.target.maxPriceExpected.value,
+            orderSize: event.target.orderSize.value,
+            grids: event.target.grids.value
+        })
+    },
+});
